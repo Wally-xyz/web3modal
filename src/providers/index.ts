0a7574e51304ec9b1946c1b67a4122dd75935a16
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-01 16:19:26 -0400

    implemented mewconnect provider config

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,19 @@
+import { IProviderInfo } from "./../helpers/types";
 import * as connectors from "./connectors";
 import * as injected from "./injected";
 import * as providers from "./providers";
+// @ts-ignore
+import MEWwallet from "./logos/mewwallet.png";
 
 export { connectors, injected, providers };
+
+export const MEWCONNECT: IProviderInfo = {
+  id: "mewconnect",
+  name: "MEW wallet",
+  logo: MEWwallet,
+  type: "web",
+  check: "isMewConnect",
+  package: {
+    required: [["infuraId", "rpc"]],
+  },
+};
