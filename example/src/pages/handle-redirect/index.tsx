8a8b4ab41a1e59d382160dfd468c568cbc64c1e8
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-11 00:07:32 -0500

    updated env

--- example/src/pages/handle-redirect/index.tsx
+++ example/src/pages/handle-redirect/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import dotenv from "dotenv";
 
 import { handleRedirect, init } from "web3modal/src/sdk";
 
 const HandleRedirect: React.FC = () => {
+  const wallyClientId = process.env.REACT_APP_WALLY_CLIENT_ID;
+
   if (typeof window !== "undefined") {
     init({
-      clientId: "15672a04-5ce6-48ff-991c-54ab200bdd5b",
+      clientId: wallyClientId!
     });
 
     handleRedirect({
       closeWindow: true,
-      appendContent: true,
+      appendContent: true
     });
   }
 
