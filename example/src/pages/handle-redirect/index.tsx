2317d65ee54bcce69e92bada7301a356967cd114
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-14 17:15:02 -0500

    removed init

--- example/src/pages/handle-redirect/index.tsx
+++ example/src/pages/handle-redirect/index.tsx
@@ -2,16 +2,10 @@ import React from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import dotenv from "dotenv";
 
-import { handleRedirect, init } from "web3modal/src/sdk";
+import { handleRedirect } from "web3modal/src/sdk";
 
 const HandleRedirect: React.FC = () => {
-  const wallyClientId = process.env.REACT_APP_WALLY_CLIENT_ID;
-
   if (typeof window !== "undefined") {
-    init({
-      clientId: wallyClientId!
-    });
-
     handleRedirect({
       closeWindow: true,
       appendContent: true
