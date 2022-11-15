f448adb58ce871933f30a0b9ab9a44df0ef00408
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-15 14:44:42 -0500

    updated sdk

--- example/src/pages/handle-redirect/index.tsx
+++ example/src/pages/handle-redirect/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import dotenv from "dotenv";
 
-import { handleRedirect } from "web3modal/src/sdk";
+import { handleRedirect } from "wally-sdk";
 
 const HandleRedirect: React.FC = () => {
   if (typeof window !== "undefined") {
     handleRedirect({
       closeWindow: true,
-      appendContent: true
+      appendContent: true,
     });
   }
 
