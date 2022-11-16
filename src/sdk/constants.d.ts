1453108de2b5ef74f24a2a893fd68cc05eb24318
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-15 21:02:44 -0500

    Cleaned up unused files

--- src/sdk/constants.d.ts
+++
@@ -1,8 +0,0 @@
-import { WallyMethodName } from "./types";
-export declare const APP_ROOT = "http://localhost:8888/v1";
-// https://api.wally.xyz/v1
-export declare const REDIRECT_CAPTION_ID = "wally-redirect-caption";
-export declare const SCRIM_TEXT_ID = "wally-scrim-text";
-export declare const getScrimElement: () => HTMLElement;
-export declare const getRedirectPage: () => HTMLElement;
-export declare const WALLY_ROUTES: Record<WallyMethodName, string>;
--- src/sdk/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { WallyConnectorOptions, RedirectOptions } from './types';
-import WallyConnector from './wally-connector';
-declare global {
-    var wally: WallyConnector;
-}
-export declare const init: (options: WallyConnectorOptions) => void;
-export declare const handleRedirect: (options: RedirectOptions) => void;
-export declare const getProvider: () => WallyConnector | null;
